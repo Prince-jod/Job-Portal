fd57fbd92fd8e9e6f4829fab0f875969dd7a5563
Prince-jod <baghelprince164@gmail.com>
2026-06-24 01:47:04 +0530

    added interRole.controller.js

--- backend/controllers/interview.controller.js
+++ backend/controllers/interview.controller.js
@@ -2,3 +2,26 @@ import InterviewCompany from "../models/interviewCompany.model.js";
 import InterviewQuestion from "../models/interviewQuestion.model.js";
 import InterviewRole from "../models/interviewRole.model.js";
 import RoleQuestion from "../models/roleQuestion.model.js";
+import {uploadFiles} from "../utils/helpers.js";
+
+//add to a company interview question
+export const addInterviewComapany=async(req,res)=>{
+  try{
+    const { companyName, questionsCount,questionData}=req.body;
+    if(!companyName || !questionsCount){
+      return res.status(400).json({
+message:"Required fields missing"
+      })
+    }
+    const exists =await InterviewCompany.findOne({companyName});
+    if(exists){
+      return res.status(400).json({
+        message: "Company already exists"
+      });
+    }
+    const uploads=await uploadFiles
+  }
+  catch(error){
+
+  }
+}
---
+++ backend/utils/helpers.js
@@ -0,0 +1,53 @@
+import {uploadToCloudinary} from "./cloudinaryUpload.js";
+
+export const uploadFiles =async(files,config)=>{
+  const result={};
+  if(!files) return result;
+  for(const key in config){
+    if(files[key]){
+      const file=files[key][0];
+
+      const uploadRes=await uploadToCloudinary(
+        file.buffer,
+        config[key].folder,
+        config[key].type,
+        file.originalname
+      );
+      result[key]=uploadRes.secure_url;
+    }
+  }
+  return result;
+}
+//parse and format question
+export const parseQuestions=(questionsData,type,id,userId)=>{
+  const parsed=JSON.parse(questionData);
+  return parsed.map((q)=>{
+    let date=new Date(q.postDate);
+    if(isNaN(date)) date=new Date();
+     return {
+            ...(type === "company" && { company: id }),
+            ...(type === "role" && { roleId: id }),
+            question: q.question,
+            answer: q.answer,
+            keyPoints: Array.isArray(q.keyPoints) ? q.keyPoints : [q.keyPoints],
+            postDate: date,
+            createdBy: userId,
+            askedBy: q.companies?.map((c) => ({
+                companyName: c.name || "",
+                dateAsked: c.date || "",
+            })) || [],
+        };
+  })
+}
+//replace all questions
+export const replaceQuestion=async(Model, filter, questions)=>{
+  await Model.deleteMany(filter);
+  await Model.insertMany(questions);
+};
+//handle error
+export const handleError=(res,rej)=>{
+  return res.status(500).json({
+    success:false,
+    message:err.message
+  })
+};
