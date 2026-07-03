61dcf7b89c2893586957a3d8ed9b9e164ab72353
Prince-jod <baghelprince164@gmail.com>
2026-07-04 03:04:44 +0530

    Auto commit

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import react from 'react'
 import Navbar from '../components/Navbar'
 import Dashboard from '../components/Dashboard'
-
+import LoginPage from '../components/LoginPage'
 const Home=()=>{
   return (
     <div>
