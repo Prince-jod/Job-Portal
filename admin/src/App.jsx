7cede919c612b99c21494f69ac272785279ea482
Prince-jod <baghelprince164@gmail.com>
2026-07-04 03:06:53 +0530

    Auto commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,6 +8,7 @@ const App=()=>{
       <div>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </div>
     </div>
