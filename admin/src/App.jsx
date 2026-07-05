e45bafeb29e8d51cfe57fdb10f1da447875b9806
Prince-jod <baghelprince164@gmail.com>
2026-07-06 01:37:18 +0530

    Auto commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import react from 'react';
 import {Route,Routes} from "react-router-dom";
 import Home from "./pages/Home";
-import LoginPage from "./pages/Login";
+import Login from "./pages/Login";
 const App=()=>{
   return (
     
