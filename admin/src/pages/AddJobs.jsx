570612c6fe4d3d51f78e55dd3578a73a6be42e40
Prince-jod <baghelprince164@gmail.com>
2026-07-06 17:11:19 +0530

    Auto commit

---
+++ admin/src/pages/AddJobs.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import AddJobsPage from '../components/AddJobsPage'
+const AddJobs=()=>{
+  return (
+    <div>
+       <AddJobsPage/>
+    </div>
+  )
+}
+export default AddJobs;
