8aca915e34a000a7fa5e0c1fee83eb485d7f531f
Prince-jod <baghelprince164@gmail.com>
2026-07-13 13:25:10 +0530

    Auto commit

---
+++ admin/src/components/ListCompanyQuestion.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+const ListCompanyQuestion = () => {
+  return (
+    <div>
+      list company question
+    </div>
+  );
+} 
+
+export default ListCompanyQuestion;
---
+++ admin/src/pages/ListCompanyQs.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import Navbar from "../components/Navbar";
+import ListCompanyQuestion from "../components/ListCompanyQuestion";  
+
+
+const ListCompanyQs = () => {
+  return (
+    <div>
+      <Navbar />
+      <ListCompanyQuestion />
+    </div>
+  );
+}
