6bdc4dec06d8b854dfeba0bb416579530ca982e7
Prince-jod <baghelprince164@gmail.com>
2026-07-13 16:07:49 +0530

    Auto commit

---
+++ admin/src/pages/ListRoleQs.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import Navbar from "../components/Navbar";
+import ListRoleQuestion from "../components/ListRoleQuestion";
+
+
+const ListRoleQs = () => {
+  return (
+    <div> 
+      <Navbar />
+      <ListRoleQuestion />
+    </div>
+  );
+}
+export default ListRoleQs;
