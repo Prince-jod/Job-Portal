acde20b3d6880c8b048f94e5b601ac1834f8da66
Prince-jod <baghelprince164@gmail.com>
2026-07-13 16:22:13 +0530

    Auto commit

---
+++ admin/src/components/ViewApplicantsPage.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import {viewApplicantsPageStyles as s} from '../assets/dummyStyles';
+
+const ViewApplicantsPage = () => {
+  return (
+    <div style={s.pagecontainer}>
+      <button>  </button>
+      </div>
+  )
+}
+export default ViewApplicantsPage;
