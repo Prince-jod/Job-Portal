4ccf1b391bca8c0a439c91b81fd8220857ba1fb6
Prince-jod <baghelprince164@gmail.com>
2026-07-14 17:13:03 +0530

    Auto commit

---
+++ admin/src/pages/ApplicantsPage.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import Navbar from "../components/Navbar";
+import ApplicantsPage from "../components/ViewApplicantsPage";
+
+const ApplicantsPage = () => {
+  return (
+  <div>
+<Navbar />
+<ApplicantsPage />
+  
+  </div>
+  )
+};
+
+export default ApplicantsPage;
